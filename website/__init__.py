72343cf194aad7e5bc1c48efadba9a85587db81a
Wafiq Aariz <wafiq.aariz@gmail.com>
2025-08-25 23:12:21 +0800

    added sqlalchemy and login_manager to run HTML

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = '123'
+    app.config['SECRET_KEY'] = '123' # username and password and local host for database#
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://USERNAME:PASS@localhost/database'
+
+    db.init_app(app)
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
     
     from .views import views
     from .auth import auth
