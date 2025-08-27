9e9f2ec1f062d2ebf8bce1948ec125f048ff12c6
Wafiq Aariz <wafiq.aariz@gmail.com>
2025-08-27 23:07:17 +0800

    added routes to display main page/home page

--- website/views.py
+++ website/views.py
@@ -1,7 +1,30 @@
 from flask import Blueprint, render_template
+from .models import User
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/home')
 def home():
-    return ('To be Done')
+    return render_template('main.html')
+
+@views.route('/posts')
+def posts():
+    return render_template('main.html')
+
+@views.route('/catalog')
+def catalog():
+    return render_template('main.html')
+
+@views.route('/map')
+def map():
+    return render_template('main.html')
+
+@views.route('/chat')
+def chats():
+    return render_template('main.html')
+
+@views.route('/users')
+def lists_users():
+    # Example: fetch users from DB
+    users = User.query.all()
+    return '<br>'.join([f"{u.user_id} - {u.name} ({u.email})" for u in users])  
