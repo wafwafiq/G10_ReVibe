b01581856838d8e53face5aa411c413e8b4cbaf0
Wafiq Aariz <wafiq.aariz@gmail.com>
2025-09-20 00:20:07 +0800

    pulled code

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template , request, redirect, url_for, current_app
+from flask import Blueprint, render_template , request, redirect, url_for, current_app, flash
 from flask_login import login_required, current_user
 from .models import User, Item
 from . import db
