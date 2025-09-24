87c044c22aa49a57cf9ad8deb9928c13e00fdc1a
adam <deathtrooper725@gmail.com>
2025-09-25 01:24:42 +0800

    fixed line issues and implemented admin features in file website, fixed issue with condition show on the top right in catalog due to style.css, fixed condition showing in catalog and details

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_socketio import SocketIO
+from flask_mail import Mail
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+socketio = SocketIO()
+mail = Mail()
 
 def create_app(): 
     app = Flask(__name__)
