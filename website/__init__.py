9cffe8bc64d5ce0c4c385737d529577ee76ac8a5
Choong Chee Chen <choongcc057@gmail.com>
2025-09-26 23:19:17 +0800

    hopefully no problem

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_socketio import SocketIO
 from flask_mail import Mail
+from flask_socketio import SocketIO
 import os
 db = SQLAlchemy()
 login_manager = LoginManager()
-socketio = SocketIO()
 mail = Mail()
+socketio = SocketIO()
 
 def create_app(): 
     app = Flask(__name__)
@@ -15,8 +15,8 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:AMwyeRlQEsnViGvcDPiUITbFeuAXaAlv@turntable.proxy.rlwy.net:47657/railway'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['UPLOAD_FOLDER'] = os.path.join('website', 'static', 'uploads')
-    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.secret_key = SECRET_KEY= your_flask_secret
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True) 
+    app.secret_key = "your_secret_key"
 
     app.config['MAIL_SERVER'] = 'smtp-relay.brevo.com'
     app.config['MAIL_PORT'] = 587
@@ -30,7 +30,7 @@ def create_app():
 
     db.init_app(app)
     mail.init_app(app)
-    socketio.init_app(app, cors_allowed_origins="*")
+    socketio.init_app(app)
 
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
@@ -46,6 +46,15 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/auth')
+
+
+    @app.after_request
+    def add_header(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
+        return response
+
 
 
     return app
