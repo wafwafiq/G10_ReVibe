204424bcb980159a96263aec1aa36166c2bd2e1f
Choong Chee Chen <choongcc057@gmail.com>
2025-09-27 02:13:22 +0800

    added things in database

--- website/models.py
+++ website/models.py
@@ -9,6 +9,8 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
+    confirmed = db.Column(db.Boolean, nullable=False, default=False)  # tinyint(1) → Boolean
+    is_admin = db.Column(db.Boolean, nullable=True, default=False)    # tinyint(1) → Boolean
     created_at = db.Column(db.TIMESTAMP, default=datetime.utcnow)
     def __repr__(self):
         return f"<User {self.name}>"
