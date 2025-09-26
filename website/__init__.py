000aa9be02b4862a297ae70d597d6d3dce73a696
Choong Chee Chen <choongcc057@gmail.com>
2025-09-26 22:15:22 +0800

    fixed email verification

--- website/__init__.py
+++ website/__init__.py
@@ -4,8 +4,6 @@ from flask_login import LoginManager
 from flask_socketio import SocketIO
 from flask_mail import Mail
 import os
-from dotenv import load_dotenv
-load_dotenv()  # loads variables from .env into os.environ
 db = SQLAlchemy()
 login_manager = LoginManager()
 socketio = SocketIO()
@@ -18,15 +16,15 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['UPLOAD_FOLDER'] = os.path.join('website', 'static', 'uploads')
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.secret_key = os.getenv("SECRET_KEY")
+    app.secret_key = SECRET_KEY= your_flask_secret
 
     app.config['MAIL_SERVER'] = 'smtp-relay.brevo.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = False
-    app.config['MAIL_USERNAME'] = os.getenv("BREVO_SMTP_USER")
-    app.config['MAIL_PASSWORD'] = os.getenv("BREVO_SMTP_KEY")
-    app.config['MAIL_DEFAULT_SENDER'] = ('No Reply - ReVibe', 'revibe.app.mail@gmail.com')  # Verified sender
+    app.config['MAIL_USERNAME'] = "97ee25002@smtp-brevo.com"
+    app.config['MAIL_PASSWORD'] = "xsmtpsib-abc60c9036f2a6cb67056e2bf7e919d77eceda81ea7a295d6e82483ba814c44e-YgaLzBQ6qjhs5RcV"
+    app.config['MAIL_DEFAULT_SENDER'] = ('No Reply - ReVibe', 'revibe.app.mail@gmail.com')
     app.config['MAIL_DEBUG'] = True
 
 
